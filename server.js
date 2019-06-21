7be20485bfac75e958be886def38cf0fda375e38
ravipun <ravipuj8@gmail.com>
2019-06-21 17:41:57 +0700

    Edit Server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = 5000;
 const server = http.createServer((req,res)=>{
     res.statusCode = 200;
     res.setHeader('Content-Type','text/plain');
-    res.end('Hello World!\n');
+    res.end('Hello World! I am a Legend\n');
 });
 
 server.listen(port,()=>{
